1eca3f5e6808f7589a40fee21b1e7437fa61b8f3
jazz-codes <152726047+jazz-codes@users.noreply.github.com>
2024-02-17 15:29:16 +0100

    pour l'execution sur 2 machines distantes

---
+++ SR_Clients/src/EX2/SocketClient.java
@@ -0,0 +1,32 @@
+package EX2;
+
+import java.io.*;
+import java.net.*;
+
+public class SocketClient {
+    public static void main(String argv[]) {
+        String host = "127.0.0.1";
+        int port = 5431;
+
+        try {
+            InetAddress adr = InetAddress.getByName(host);
+            Socket socket = new Socket(adr, port);
+            ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
+            ObjectInputStream input = new ObjectInputStream(socket.getInputStream());
+
+            Voiture voiture = new Voiture("Mustang", "Ford ");
+            output.writeObject(voiture);
+            Voiture voitureModifiee = (Voiture) input.readObject();
+            System.out.println("Voiture reçue du serveur : " + voitureModifiee.getType() + " " +
+                    voitureModifiee.getModel() + ", carburant : " + voitureModifiee.getCarburant() + "/" +
+                    voitureModifiee.getCapacite());
+
+            input.close();
+            output.close();
+            socket.close();
+
+        } catch (Exception e) {
+            System.err.println("Erreur : " + e);
+        }
+    }
+}
